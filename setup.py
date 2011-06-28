f485c23336d4f19a14b3d6413bc1fc23c875bc6c
Calen Pennington <cpennington@wgen.net>
2011-06-28 11:57:09 -0400

    Add entry to setup.py to install the new gnuplot templates

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
                         'psutil',
                         'mako >= 0.4.1'],
     zip_safe=True,
-    package_data={'funkload': ['data/*',
+    package_data={'funkload': ['data/templates/gnuplot/*', 'data/*',
                                'demo/simple/*', 'demo/zope/*',
                                'demo/cmf/*', 'demo/xmlrpc/*', 'demo/cps/*',
                                'demo/seam-booking-1.1.5/*', 'demo/*.txt',
